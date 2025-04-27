2dbab7c90bf4745187cad6ff35e25ae0f4f5f628
Austin Almond <aust.almond@gmail.com>
2025-04-26 23:31:40 -0400

    Day 1 part 2

--- AdventOfCode2024/Day1/Day1.fsx
+++ AdventOfCode2024/Day1/Day1.fsx
@@ -1,9 +1,9 @@
 ﻿// https://adventofcode.com/2024/day/1
 
-module Part1 =
+module Common =
     open System.IO
 
-    let solve fileName =
+    let inputLists fileName =
         let dir = __SOURCE_DIRECTORY__
         let inFile = $"{dir}/{fileName}"
         let fileContents = File.ReadAllLines inFile
@@ -11,13 +11,32 @@ module Part1 =
         let parseLine (line: string) =
             line.Split("   ") |> Array.map int |> (fun a -> (a.[0], a.[1]))
 
-        let distance pair = fst pair - snd pair |> abs
+        fileContents |> Array.map parseLine |> Array.unzip
 
-        let left, right = fileContents |> Array.map parseLine |> Array.unzip
+module Part1 =
+    let solve fileName =
+        let distance pair = fst pair - snd pair |> abs
+        let left, right = Common.inputLists fileName
 
         Array.zip (Array.sort left) (Array.sort right)
         |> Array.map distance
         |> Array.sum
+
+    let exampleResult = solve "example_input.txt"
+    let fullResult = solve "input.txt"
+
+module Part2 =
+    let solve fileName =
+        let left, right = Common.inputLists fileName
+
+        let counts = right |> Array.countBy id |> Map
+
+        let countOrZero n =
+            match counts.TryFind n with
+            | Some n -> n
+            | None -> 0
+
+        left |> Array.map (fun n -> n * countOrZero n) |> Array.sum
 
     let exampleResult = solve "example_input.txt"
     let fullResult = solve "input.txt"
