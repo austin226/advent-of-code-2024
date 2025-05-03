40e7ae21b931c6dafc326d5dec7b9e2d8c0fbce1
Austin Almond <aust.almond@gmail.com>
2025-05-02 22:53:33 -0400

    Solve Day 2 Part 2

--- AdventOfCode2024/Day2/Day2.fsx
+++ AdventOfCode2024/Day2/Day2.fsx
@@ -10,15 +10,14 @@ module Common =
 
         fileContents
 
-module Part1 =
-    let safe levels =
-        let allPairs condition array =
-            array |> Array.pairwise |> Array.map condition |> Array.forall id
+    let safe report =
+        let allPairs condition =
+            report |> Array.pairwise |> Array.map condition |> Array.forall id
 
-        let monotonic levels =
+        let monotonic =
             let allIncreasing = allPairs (fun p -> fst p <= snd p)
             let allDecreasing = allPairs (fun p -> fst p >= snd p)
-            allIncreasing levels || allDecreasing levels
+            allIncreasing || allDecreasing
 
         let allGradual =
             let gradualChange pair =
@@ -27,7 +26,30 @@ module Part1 =
 
             allPairs gradualChange
 
-        monotonic levels && allGradual levels
+        monotonic && allGradual
+
+module Part1 =
+    let solve fileName =
+        let parseReport line = line |> Array.map int
+
+        let reports =
+            Common.input fileName |> Array.map _.Split(" ") |> Array.map parseReport
+
+        reports |> Array.filter Common.safe |> Array.length
+
+
+    let exampleResult = solve "example_input.txt"
+    let fullResult = solve "input.txt"
+
+module Part2 =
+    let rmIdx (report: int array) i =
+        report
+        |> Array.mapi (fun j x -> if i <> j then Some x else None)
+        |> Array.choose id
+
+    let alts (report: int array) = Seq.init report.Length (rmIdx report)
+
+    let almostSafe (report: int array) = alts report |> Seq.exists Common.safe
 
     let solve fileName =
         let parseReport line = line |> Array.map int
@@ -35,8 +57,7 @@ module Part1 =
         let reports =
             Common.input fileName |> Array.map _.Split(" ") |> Array.map parseReport
 
-        reports |> Array.filter safe |> Array.length
-
+        reports |> Array.filter almostSafe |> Array.length
 
     let exampleResult = solve "example_input.txt"
     let fullResult = solve "input.txt"
